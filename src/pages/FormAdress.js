36b116bdcf2e18c2cf2c668eef55016005676605
guipiri <gui.soliveiras@gmail.com>
2022-09-23 19:07:11 -0300

    página de forms e validação de cep com api da viaCep

--- src/pages/FormAdress.js
+++ src/pages/FormAdress.js
@@ -1,22 +1,85 @@
 import React from "react";
+import { useState } from "react";
 import styles from "./FormAdress.module.css";
 
 function FormAdress() {
+	const [cep, setCep] = useState("")
+	const [adress, setAdress] = useState("")
+
+	function handleOnChange(e) {
+		console.log(cep)
+		setCep(e.target.value)
+	}
+
+	function handleChange(e) {
+		setAdress({ ...adress, [e.target.name]: e.target.value })
+		console.log(adress)
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		setAdress("")
+		fetch(`https://viacep.com.br/ws/${cep}/json/`)
+			.then((res) => res.json())
+			.then((data) => { setAdress(data) })
+			.catch(() => { alert("Digite um CEP válido!"); setCep(""); })
+
+	}
 	return (
 		<div className={styles.conteiner}>
-			<form className={styles.form}>
-				<label for="CEP">
-					Digite o CEP para a entrega dos bonecos:
-				</label>
-				<input
-					type="number"
-					id="CEP"
-					name="CEP"
-					placeholder="012345678"
-					className={styles.inputCep}
-				/>
+			<form className={styles.form} onSubmit={handleSubmit}>
+				<div>
+
+					<label htmlFor="CEP">
+						Digite o CEP para a entrega dos bonecos:
+					</label>
+					<input
+						type="number"
+						id="CEP"
+						name="cep"
+						value={cep}
+						placeholder="012345678"
+						className={styles.inputCep}
+						onChange={handleOnChange}
+					/>
+				</div>
 			</form>
-		</div>
+			{adress && (
+				<form>
+					<table>
+						<tr>
+							<td colSpan="2">
+								<label htmlFor="logradouro">Logradouro:</label>
+								<input type="text" onChange={handleChange} defaultValue={adress.logradouro} className={`${styles.inputCep} ${styles.grande}`} id="logradouro" name="logradouro" />
+							</td>
+							<td>
+								<label htmlFor="numero">Nº:</label>
+								<input type="number" onChange={handleChange} placeholder="1234" className={`${styles.inputCep} ${styles.pequeno}`} id="numero" name="numero" />
+							</td>
+						</tr>
+						<tr>
+
+							<td>
+								<label htmlFor="bairro">Bairro:</label>
+								<input type="text" onChange={handleChange} defaultValue={adress.bairro} className={styles.inputCep} id="bairro" name="bairro" />
+							</td>
+						</tr>
+						<tr>
+							<td colSpan="2">
+								<label htmlFor="cidade">Cidade:</label>
+								<input type="text" onChange={handleChange} defaultValue={adress.localidade} className={styles.inputCep} id="cidade" name="localidade" />
+							</td>
+							<td>
+								<label htmlFor="uf">UF:</label>
+								<input type="text" onChange={handleChange} defaultValue={adress.uf} className={`${styles.inputCep} ${styles.pequeno}`} id="uf" name="uf" />
+							</td>
+						</tr>
+					</table>
+				</form>
+			)
+			}
+
+		</div >
 	);
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,16 +10,19 @@ function Header() {
 				<img
 					id={styles.logosb}
 					src="https://live.sportsbet.io/assets/landings/logo-sportsbet-light-529416dd25e42996300ad554b27c22244502904174be32ea6dfb4df08a170af0.png"
+					alt="logomarca-sportsbet"
 				/>
 				<img
 					id={styles.logoimperial}
 					src="https://seeklogo.com/images/I/imperial-esports-logo-728A269FBB-seeklogo.com.png?v=637810384520000000"
+					alt="logomarca-imperial-esports"
 				/>
 			</div>
 			<div>
 				<h1>Resgate seus bonecos da Imperial Esports aqui!</h1>
 				<InputCodigo />
-				<img id={styles.jogadores} src={img} />
+				<img id={styles.jogadores} src={img} alt="jogadores-imperial"
+				/>
 			</div>
 		</div>
 	);
